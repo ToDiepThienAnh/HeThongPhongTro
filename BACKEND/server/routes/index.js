02e85317ab25e65e6e4ee6203c498ab127c1eac6
unknown <todiepthienanh@gmail.com>
2020-12-01 21:07:12 +0700

    'Done-setUp-Project'

--- BACKEND/server/routes/index.js
+++ BACKEND/server/routes/index.js
@@ -1,9 +1,42 @@
 var express = require('express');
 var router = express.Router();
 
+
+const { Pool, Client } = require('pg')
+
+
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'HeThongPhongTro',
+  password: 'khongtrihoan',
+  port: 5432,
+})
+
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  pool.query('SELECT NOW()', (err, res) => {
+    console.log(err, res)
+    pool.end()
+  })
   res.render('index', { title: 'Express' });
 });
+
+
+router.get('/getdata01', function(req, res, next) {
+  pool
+  .query('SELECT * FROM taikhoan')
+  .then(respond => res.send(respond.rows))
+  .catch(error =>
+    setImmediate(() => {
+      throw error
+    })
+  )
+  
+});
+
+
 
 module.exports = router;
